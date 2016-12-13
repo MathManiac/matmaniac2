e24608161f6fbffe4ddd3d5b250f2719acac0c89
Henrik Hansen <lianides@hotmail.com>
2016-12-13 22:51:33 +0100

    Så virker XInOneSide

--- app/Opgaver/Equations/XOnOneSide.php
+++ app/Opgaver/Equations/XOnOneSide.php
@@ -10,7 +10,7 @@ class XOnOneSide extends Question implements QuestionInterface, ResultInterface
 {
     public function Questions()
     {
-        $a = rand(2,15);
+        //$a = rand(2,15);
         $b = rand(2,15);
         $c = rand(1,5);
         $d = rand(6,11);
@@ -37,43 +37,43 @@ class XOnOneSide extends Question implements QuestionInterface, ResultInterface
     public function validateQuestion($input, $question)
     {
         $input = (float)$input['result'];
-
+        
         switch ($question['type']) {
 
             case 1:
                 //$a = $question['numbers'][0];
-                $b = $question['numbers'][1];
-                $c = $question['numbers'][2];
+                $b = $question['numbers'][0];
+                $c = $question['numbers'][1];
                 //$d = $question['numbers'][3];
                 $res = round($b - $c, 2);
-                \Debugbar::addMessage($res, 'Resultat');
+                \Debugbar::addMessage($res, 'result');
                 \Debugbar::addMessage($input, 'Input');
                 return ['result' => $res == round($input, 2)];
             case 2:
                 //$a = $question['numbers'][0];
-                $b = $question['numbers'][1];
-                $c = $question['numbers'][2];
+                $b = $question['numbers'][0];
+                $c = $question['numbers'][1];
                 //$d = $question['numbers'][3];
                 $res = round($b+$c,2);
-                \Debugbar::addMessage($res, 'Resultat');
+                \Debugbar::addMessage($res, 'result');
                 \Debugbar::addMessage($input, 'Input');
                 return ['result' => $res == round($input, 2)];
             case 3:
                 //$a = $question['numbers'][0];
-                $b = $question['numbers'][1];
-                $c = $question['numbers'][2];
+                $b = $question['numbers'][0];
+                $c = $question['numbers'][1];
                 //$d = $question['numbers'][3];
                 $res = round($c-$b,2);
-                \Debugbar::addMessage($res, 'Resultat');
+                \Debugbar::addMessage($res, 'result');
                 \Debugbar::addMessage($input, 'Input');
                 return ['result' => $res == round($input, 2)];
             case 4:
                 //$a = $question['numbers'][0];
-                $b = $question['numbers'][1];
-                $c = $question['numbers'][2];
-                $d = $question['numbers'][3];
+                $b = $question['numbers'][0];
+                $c = $question['numbers'][1];
+                $d = $question['numbers'][2];
                 $res = round(($b-$c)/$d,2);
-                \Debugbar::addMessage($res, 'Resultat');
+                \Debugbar::addMessage($res, 'result');
                 \Debugbar::addMessage($input, 'Input');
                 return ['result' => $res == round($input, 2)];
         }
